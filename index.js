2bfd2c84e47b0682b8829923a5c09c132d30a7d9
Samuelbilu <ssantiagocv@gmail.com>
2021-07-17 17:41:57 -0300

    Adding chat

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
+//requires
 const favicon = require("serve-favicon");
 const path = require('path');
-const express = require("express");
+const express = require('express');
 const app = express();
+const http = require('http');
+const server = http.createServer(app)
+
+//socket.io
+
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+//variaveis
 
 const port = 80
+
+//Páginas normais
 
 app.get("/", (req, res) => {
     res.render("index.ejs")
@@ -17,8 +29,23 @@ app.get("/register", (req, res) => {
     res.render("register.ejs")
 });
 
+//io
+
+io.on('connection', (socket) => {
+    socket.broadcast.emit('hi');
+});
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+      io.emit('chat message', msg);
+    });
+});
+io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' })
+
+//universal
+
 app.use(favicon(path.join(__dirname, 'favicon.ico')))
 
-app.listen(process.env.PORT || port, () =>{
+server.listen(process.env.PORT || port, () =>{
     console.log("Express server listening")
 });

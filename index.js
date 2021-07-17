cd38eb0bc2e35504a18a6ae43d185c4dda892a57
Samuelbilu <ssantiagocv@gmail.com>
2021-07-17 16:37:06 -0300

    Solveing Problems

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const { render } = require("ejs");
-const dotenv = require("dotenv").config();
 const favicon = require("serve-favicon");
 const path = require('path');
 const express = require("express");
@@ -8,11 +6,11 @@ const app = express();
 const port = 80
 
 app.get("/", (req, res) => {
-    render("index.ejs")
+    res.send("testando")
 });
 
 app.get("/login", (req, res) => {
-    render("login.ejs")
+    res.render("login.ejs")
 });
 
 app.use(favicon(path.join(__dirname, 'favicon.ico')))

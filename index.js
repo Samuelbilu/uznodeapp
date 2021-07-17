d46da4cfd71332bb9a5a2a9aa6611d0d41217216
Samuelbilu <ssantiagocv@gmail.com>
2021-07-17 16:40:07 -0300

    Solving Problems

--- index.js
+++ index.js
@@ -6,11 +6,15 @@ const app = express();
 const port = 80
 
 app.get("/", (req, res) => {
-    res.send("testando")
+    res.render("index.ejs")
 });
 
 app.get("/login", (req, res) => {
     res.render("login.ejs")
+});
+
+app.get("/register", (req, res) => {
+    res.render("register.ejs")
 });
 
 app.use(favicon(path.join(__dirname, 'favicon.ico')))

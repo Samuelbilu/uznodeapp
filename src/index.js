a4ba5c297cfb694aeb4c1bdddc686c21b05cf81c
Samuelbilu <ssantiagocv@gmail.com>
2021-07-29 15:56:19 -0300

    salvando as msg

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@
 const favicon = require("serve-favicon");
 const path = require('path');
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyparser = require('body-parser');
+const { Msg } = require('./models/message');
 const app = express();
 const http = require('http');
 const server = http.createServer(app)
@@ -20,27 +22,47 @@ const io = new Server(server);
 //variaveis
 
 const port = 80;
+const mongoDB = 'mongodb+srv://samuz0:uz0reinando@firstcluster.gpdqi.mongodb.net/chatdb?retryWrites=true&w=majority'
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true }).then(()=>{
+    console.log('mongodb connected');
+}).catch((err) => { 
+    console.log("erro: " + err); 
+});
+
 
 //io
 
+const users = {};
+
 io.on('connection', (socket) => {
 
-    socket.on("userConnected", (name) =>{
-        io.emit("userConnected", name)
+    socket.on("userConnected", (userId) =>{
+        io.emit('userConnected', { userId, users })
     })
 
     socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
-    });
-    io.on('disconnect', (socket)=>{
-        socket.on('userDisconnected', (name) =>{
-            io.emit("userDisconnected", name);
+        const message = new Msg({msg});
+        message.save().then(()=>{
+            io.emit('chat message', msg);
         });
-    })
+    });
+    socket.on('disconnect', function(){
+        console.log('user ' + users[socket.id] + ' disconnected');
+        delete users[socket.id];
+
+        io.emit('disconnection', users)
+
+    });
+
+    
 });
 // io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' })
 
 //universal
+
+module.exports = {
+    users
+}
 
 app.use(favicon(path.join(__dirname, 'favicon.ico')));
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,6 @@
 const { Router } = require("express");
 const router = Router();
 
-//usuários
-
-//const { createUser, verifyUser, users } = require("../controllers/index.controller");
 
 router.get("/", (req, res) => {
     res.render("index.ejs");
@@ -11,32 +8,12 @@ router.get("/", (req, res) => {
 
 router.get("/login", (req, res) => {
     //res.render("login.ejs");
-    res.send("página em teste")
+    res.send("página em teste");
 });
-
-/*router.post("/login", (req, res) => {
-    const { username, password} = req.body;
-
-    verifyUser(username, password);
-    if(verifyUser){
-        res.send("logged")
-    }
-});*/
 
 router.get("/register", (req, res) => {
     //res.render("register.ejs");
-    res.send("pagina em teste")
+    res.send("pagina em teste");
 });
-
-router.post("/register", (req, res) => {
-    //const { username, password} = req.body;
-
-    //createUser(username, password);
-});
-
-//router.get("/users", users)
-
-
-
 
 module.exports = router;
